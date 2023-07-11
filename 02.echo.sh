23624a7da76fb5a0bcf6c1c4a0258d6586882e65
mohan <mohanprasad390@gmail.com>
2023-07-11 14:56:20 +0530

    echo

--- 02.echo.sh
+++ 02.echo.sh
@@ -2,11 +2,11 @@ echo Hellow world
 
 #  Enable Color
 #  syntax →  echo -e “\e[COLmMESSAGE\e[0m”
-#- e → enable \e (enable color in echo command)
+#  e → enable \e (enable color in echo command)
 #  All the input should be there in quotes (Double quotes preferred)
 #  \e  → Enable a color
 #  COLm → Color code -  **Red(31m),  Green(32m),  yellow(33m), Blue(34m), Magenta(35m), cyan(36m).
 #  \e[0m → Disable the color. (Once we enable the color we have to disable that, else color follows to next lines.)
 
-echo -e "\e[31mRED Color"
+echo -e "\e[31mRED Color\e[0m"
 echo Normal Text
