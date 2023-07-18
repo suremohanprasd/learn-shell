b4fd5b03178b1f20cc2d4610146da4cd86664531
mohan <mohanprasad390@gmail.com>
2023-07-18 21:13:10 +0530

    DATE

--- 03.variable.sh
+++ 03.variable.sh
@@ -10,4 +10,12 @@ DATE=$(date +%F)
 echo Today date is ${DATE}
 
 ARTH=$((2-3*4/2))
+
 echo ARTH = ${ARTH}
+
+# Special Variables for Inputs
+echo Script Name - $0
+echo First Argument - $1
+echo Second Argument - $2
+echo All Argument - $0
+echo No of Arguments - $#
