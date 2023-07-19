9d01b465f660ce2a36a3ec0d9239cc08c4bebe0a
mohan <mohanprasad390@gmail.com>
2023-07-19 13:14:26 +0530

    DATE

--- 05.if.sh
+++ 05.if.sh
@@ -1,9 +1,18 @@
 fruit_name=$1
 quantity=$2
 
-if [ "$fruit_name" == "banana" ]
+if [ "$fruit_name" == "mango" ]
 then
   echo Mango Quantity - $quantity
 else
   echo Fruit does not exist
+fi
+
+## Always double quote variables in expressions
+
+if [ "quantity" -gt 100]
+then
+  echo mango price - $0.5
+else
+  echo mango price - $1
 fi
